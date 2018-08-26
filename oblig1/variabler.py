88d74fc73ecde862ad518707ddc7b9828720b257
Jon Zwaig Kolstad <jon.zk@hotmail.com>
2018-08-26 18:16:10 +0200

    Improve oppg2.1

--- oblig1/variabler.py
+++ oblig1/variabler.py
@@ -1,6 +1,6 @@
-variable_1 = 6
-variable_2 = 9
+variable_1 = 2
+variable_2 = 4
 
 # F-strings requires Python 3.6
-print(f"{variable_1}\n{variable_2}")
+print(f"Variable no. 1: {variable_1}\nVariable no. 2: {variable_2}")
 
