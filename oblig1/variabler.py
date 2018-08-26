06646bfc44a05513ea9ad56162ea061b94865f07
Jon Zwaig Kolstad <jon.zk@hotmail.com>
2018-08-26 18:21:18 +0200

    Change to complete oppg2.2

--- oblig1/variabler.py
+++ oblig1/variabler.py
@@ -1,6 +1,7 @@
 variable_1 = 2
 variable_2 = 4
+summen = variable_1 + variable_2
 
 # F-strings requires Python 3.6
-print(f"Variable no. 1: {variable_1}\nVariable no. 2: {variable_2}")
+print(f"Variable no. 1: {variable_1}\nVariable no. 2: {variable_2}\nSum: {summen}")
 
