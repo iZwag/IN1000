635c3411e2058e5e0472e8b96080329100abdcfb
Jon Zwaig Kolstad <jon.zk@hotmail.com>
2018-08-27 21:05:38 +0200

    Add comments to oppg3

--- oblig1/beslutninger.py
+++ oblig1/beslutninger.py
@@ -1,5 +1,7 @@
-svar = input("Kunne du tenke deg en brus? (ja/nei) ")
+# Stiller brukeren et spørsmål og lagrer svaret
+svar = input("Kunne du tenke deg en brus? \"ja\"/\"nei\" + ENTER ")
 
+# Sjekker svaret og gir korrekt tilbakemelding
 if svar == "ja":
     print("Her har du en brus!")
 elif svar == "nei":
