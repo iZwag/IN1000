ae5a81850481a300894803f25484d2bdfaf3029d
Jon Zwaig Kolstad <jon.zk@hotmail.com>
2018-08-27 20:53:06 +0200

    Change to add oppg2.3 og 2.4

--- oblig1/variabler.py
+++ oblig1/variabler.py
@@ -1,7 +1,26 @@
+# To variabler med heltallsverdier
 variable_1 = 2
 variable_2 = 4
-summen = variable_1 + variable_2
 
-# F-strings requires Python 3.6
-print(f"{variable_1}\n{variable_2}\nSum: {summen}")
+# Summen av de to variablene 
+sum = variable_1 + variable_2
 
+# Printer variablene på hver sin linje, og summen til slutt.
+# F-strings krever Python 3.6
+print(f"{variable_1}\n{variable_2}\nSum: {sum}")
+
+# To tekstvariabler
+variable_3 = "Marco"
+variable_4 = "Polo"
+
+# Legge tekstvariablene sammen
+sammen = variable_3 + variable_4
+
+# Printer tekstvariablene på hver sin linje, inkludert sammen
+print(f"{variable_3}\n{variable_4}\nSammen: {sammen}")
+
+# Lager en ny sammen-tekst med mellomrom mellom
+sammen = variable_3 + " " + variable_4
+
+# Printer den nye sammen-teksten
+print(f"Sammen: {sammen}")
