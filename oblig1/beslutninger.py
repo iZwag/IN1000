a167ac7d9460dd6f0cf166c895085c4690a94870
Jon Zwaig Kolstad <jon.zk@hotmail.com>
2018-08-27 21:00:57 +0200

    Add oppg3.1 and oppg3.2

---
+++ oblig1/beslutninger.py
@@ -0,0 +1,9 @@
+svar = input("Kunne du tenke deg en brus? (ja/nei) ")
+
+if svar == "ja":
+    print("Her har du en brus!")
+elif svar == "nei":
+    print("Den er grei.")
+else:
+    print("Det forstod jeg ikke helt.")
+
