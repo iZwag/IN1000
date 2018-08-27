d425d561092607033b3e63031c34891582598bfa
Jon Zwaig Kolstad <jon.zk@hotmail.com>
2018-08-27 21:06:47 +0200

    Add comments to oppg1

--- oblig1/hei_student.py
+++ oblig1/hei_student.py
@@ -1,3 +1,5 @@
+# Spør brukeren om navnet og lagrer det i en variabel
 navn = input("Skriv navnet ditt her og trykk enter: ")
 
+# Skriver ut en tilbakemelding til brukeren og bruker navnet dens
 print ("Hei " + navn + "!")
