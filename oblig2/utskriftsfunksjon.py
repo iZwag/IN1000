ef12f4cf104289667ded6ca5c73f5f5cb6d39ebb
Jon Zwaig Kolstad <jon.zk@hotmail.com>
2018-09-03 17:11:41 +0200

    Change to complete oppg1.2 with comments

--- oblig2/utskriftsfunksjon.py
+++ oblig2/utskriftsfunksjon.py
@@ -1,3 +1,14 @@
-navn = input("Skriv inn navn: ")
-bosted = input("Hvor kommer du fra? ")
-print("Hei, {}! Du er fra {}".format(navn, bosted))
+# Lag prosedyre ut av første deloppgave
+def navn_og_sted():
+    # Tar inn et navn
+    navn = input("Skriv inn navn: ")
+    
+    # Tar inn bosted
+    bosted = input("Hvor kommer du fra? ")
+    
+    # Hilser på personen og minner dem på hvor de er fra
+    print("Hei, {}! Du er fra {}".format(navn, bosted))
+
+# Kjører prosedyren 3 ganger
+for i in range(0,3):
+    navn_og_sted()
