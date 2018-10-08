5b6ca30929169225a2d577f7b846fbf2c06b3307
Jon Zwaig Kolstad <jon.zk@hotmail.com>
2018-10-08 20:53:36 +0200

    Add complete oppg1 with extra file

--- oblig6/telefonsalg.py
+++ oblig6/telefonsalg.py
@@ -1,7 +1,7 @@
 # Norwegian is only kept for compliance with task
 
-# Function for reading a file of cold calling seller stats
-def innlesing(filnavn)
+# Reads a file of cold calling seller stats and converts to dictionary
+def innlesing(filnavn):
     
     sales_stats = {}
     
@@ -9,10 +9,57 @@ def innlesing(filnavn)
 
     for line in f:
         stat = line.split()
-        sales_stat{stat[0]} = int(stat[1])
+        sales_stats[stat[0]] = int(stat[1])
     
     f.close()
 
     return sales_stats
 
+# Prints the seller with the highest sales given a dict with seller statistics
+def maanedensSalgsperson(seller_stats):
+    
+    sales = 0
+    
+    for k, v in seller_stats.items():
+        if v > sales:
+            seller = k
+            sales = v
 
+    print("Maanedens ansatte er %s med %d salg!" % (seller, sales))
+
+# Returns the total sales given a dict with seller statistics
+def totaltAntallSalg(seller_stats):
+    
+    sales = 0
+
+    for val in seller_stats.values():
+        sales += int(val)
+
+    return sales
+
+# Returns the average sales per seller given a dict of seller statistics
+def gjennomsnittSalg(seller_stats):
+    
+    sales= totaltAntallSalg(seller_stats)
+    
+    sellers = len(seller_stats.keys())
+    
+    avg = float(sales/sellers)
+    
+    return avg
+    
+# Main function
+def hovedprogram():
+    
+    fil = "salgstall.txt"
+    
+    stats = innlesing(fil)    
+
+    maanedensSalgsperson(stats)
+    
+    print("Aktive selgere denne maaneden: %d" % len(stats.keys()))
+    print("Totalt antall salg: %d" % totaltAntallSalg(stats))
+    print("Gjennomsnittlig antall salg per salgsperson: %.2f" % gjennomsnittSalg(stats))
+
+# Runs the main
+hovedprogram()
